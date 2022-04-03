1a3f98372ae3f20415544cd4f1786b799e8920d0
i-colours-u <i.colours.u@gmail.com>
2022-04-03 21:33:00 +0900

    [feat] 최초 프로젝트 생성
    .gitignore 파일 수정
    issue template 추가
    pull request template 추가
    xcode 프로젝트 생성

---
+++ ReadMe-iOS/ReadMe-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadMe-iOS
+//
+//  Created by 송지훈 on 2022/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
