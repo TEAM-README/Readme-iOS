e9108f41c709187260a0e34ddf24a17a4b0f1eff
i-colours-u <i.colours.u@gmail.com>
2022-04-12 16:23:34 +0900

    [feat] TabbarView 완성

---
+++ ReadMe-iOS/ReadMe-iOS/Presentation/BaseScene/Views/Tabbar/Tabbar.swift
@@ -0,0 +1,32 @@
+//
+//  Tabbar.swift
+//  ReadMe-iOS
+//
+//  Created by 송지훈 on 2022/04/12.
+//
+
+import UIKit
+
+final class TabbarView: XibView{
+  
+  private var currentTab: TabbarIconType = .home {
+    didSet{ setTabbarViewModel() }
+  }
+  
+  @IBOutlet weak var homeIcon: TabbarIcon!
+  @IBOutlet weak var mypageIcon: TabbarIcon!
+  
+  @IBAction func homeButtonClicked(_ sender: Any) {
+    currentTab = .home
+  }
+  
+  @IBAction func myPageClicked(_ sender: Any) {
+    currentTab = .mypage
+  }
+  
+  private func setTabbarViewModel() {
+    homeIcon.viewModel = TabbarIconViewModel(type: .home, clicked: currentTab == .home)
+    
+    mypageIcon.viewModel = TabbarIconViewModel(type: .mypage, clicked: currentTab == .mypage)
+  }
+}
