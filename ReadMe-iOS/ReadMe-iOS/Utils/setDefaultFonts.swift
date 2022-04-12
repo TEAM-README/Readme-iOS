d3814e21ca57bb6a43104ea09189450c1026c2ac
i-colours-u <i.colours.u@gmail.com>
2022-04-12 14:45:05 +0900

    [style] 기본 폰트 세팅 완료

--- ReadMe-iOS/ReadMe-iOS/Utils/setDefaultFonts.swift
+++ ReadMe-iOS/ReadMe-iOS/Utils/setDefaultFonts.swift
@@ -9,14 +9,13 @@ import Foundation
 import UIKit
 
 struct AppFontName {
-    static let bold = "SpoqaHanSansNeo-Bold"
-    static let regular = "SpoqaHanSansNeo-Regular"
-    static let light = "SpoqaHanSansNeo-Regular"
+    static let bold = "Pretendard-Bold"
+    static let regular = "Pretendard-Regular"
+    static let light = "Pretendard-Regular"
 }
 
 extension UIFontDescriptor.AttributeName {
     static let nsctFontUIUsage = UIFontDescriptor.AttributeName(rawValue: "NSCTFontUIUsageAttribute")
-
 }
 
 extension UIFont {
@@ -73,6 +72,43 @@ extension UIFont {
            let initCoderMethod = class_getInstanceMethod(self, #selector(UIFontDescriptor.init(coder:)))
            let myInitCoderMethod = class_getInstanceMethod(self, #selector(UIFont.init(myCoder:)))
            method_exchangeImplementations(initCoderMethod!, myInitCoderMethod!)
+    }
+  }
+}
+
+enum ReadmeFontType {
+  case thin
+  case extraLight
+  case light
+  case regular
+  case medium
+  case semiBold
+  case bold
+  case extraBold
+  case black
+}
+
+extension UIFont {
+  static func readMeFont(size: CGFloat, type: ReadmeFontType = .regular) -> UIFont{
+    switch(type) {
+      case .thin:
+        return UIFont(name: "Pretendard-Thin", size: size)!
+      case .extraLight:
+        return UIFont(name: "Pretendard-ExtraLight", size: size)!
+      case .light:
+        return UIFont(name: "Pretendard-Light", size: size)!
+      case .regular:
+        return UIFont(name: "Pretendard-Regular", size: size)!
+      case .medium:
+        return UIFont(name: "Pretendard-Medium", size: size)!
+      case .semiBold:
+        return UIFont(name: "Pretendard-SemiBold", size: size)!
+      case .bold:
+        return UIFont(name: "Pretendard-Bold", size: size)!
+      case .extraBold:
+        return UIFont(name: "Pretendard-ExtraBold", size: size)!
+      case .black:
+        return UIFont(name: "Pretendard-Black", size: size)!
     }
   }
 }
