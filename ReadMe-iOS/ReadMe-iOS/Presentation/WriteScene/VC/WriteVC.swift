f59db2cb4e26625a93a4bbf6fb267a4267b77b73
Subin Yang <cindy010670@gmail.com>
2022-04-19 22:40:52 +0900

    [feat] 글 작성 뷰 UI 요소 추가

--- ReadMe-iOS/ReadMe-iOS/Presentation/WriteScene/VC/WriteVC.swift
+++ ReadMe-iOS/ReadMe-iOS/Presentation/WriteScene/VC/WriteVC.swift
@@ -7,16 +7,54 @@
 
 import UIKit
 
+import SnapKit
+
+@frozen
+enum flowType {
+  case firstFlow
+  case secondFlow
+}
+
 class WriteVC: UIViewController {
   
   // MARK: - Vars & Lets Part
+  private let topBgView = UIView()
+  private let cheerLabel = UILabel()
+  private let describeLabel = UILabel()
+  private let titleLabel = UILabel()
+  private let writeTextView = UITextView()
+  private let nextButton = BottomButton()
+  
+  private let firstView = UIView()
+  private let firstTitleLabel = UILabel()
+  private let bookCoverImageView = UIImageView()
+  private let categoryLabel = UILabel()
+  private let bookTitleLabel = UILabel()
+  private let bookAuthorLabel = UILabel()
+  
+  private let secondView = UIView()
+  private let secondTitleLabel = UILabel()
+  private let sentenceTextView = UITextView()
+  
+  lazy var progressView = UIProgressView()
   var viewModel: WriteViewModel!
   
   // MARK: - Vars & Lets Part
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    
   }
 
+}
+
+// MARK: - UI & Layout Part
+
+extension WriteVC {
+  private func configureUI() {
+    
+  }
+  
+  private func setLayout() {
+    
+  }
 }
