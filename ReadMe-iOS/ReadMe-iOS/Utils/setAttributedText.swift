f3d101cf4ebe327c3271901d98f8bade5c30508a
Subin Yang <cindy010670@gmail.com>
2022-04-24 16:24:55 +0900

    [fix] conflict files solved

--- ReadMe-iOS/ReadMe-iOS/Utils/setAttributedText.swift
+++ ReadMe-iOS/ReadMe-iOS/Utils/setAttributedText.swift
@@ -14,7 +14,6 @@ extension UILabel {
     let fullText = self.text ?? ""
     let range = (fullText as NSString).range(of: targetString)
     let attributedString = NSMutableAttributedString(string: fullText)
-    attributedString.addAttributes([.font : font, .foregroundColor : color], range: range)
     attributedString.addAttribute(.font, value: font, range: range)
     if let textColor = color {
        attributedString.addAttribute(.foregroundColor, value: textColor, range: range)
