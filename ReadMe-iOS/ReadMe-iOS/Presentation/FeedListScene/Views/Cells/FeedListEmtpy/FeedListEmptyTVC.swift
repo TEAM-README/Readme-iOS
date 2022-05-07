df7cdd9824a5863a643f7c5237284bfc4813d4c1
i-colours-u <i.colours.u@gmail.com>
2022-05-07 23:55:54 +0900

    FeedListEmptyView xib 생성

---
+++ ReadMe-iOS/ReadMe-iOS/Presentation/FeedListScene/Views/Cells/FeedListEmtpy/FeedListEmptyTVC.swift
@@ -0,0 +1,23 @@
+//
+//  FeedListEmptyTVC.swift
+//  ReadMe-iOS
+//
+//  Created by 송지훈 on 2022/05/07.
+//
+
+import UIKit
+
+class FeedListEmptyTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ ReadMe-iOS/ReadMe-iOS/Presentation/FeedListScene/Views/EmptyViews/FeedListEmptyView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedListEmptyView.swift
+//  ReadMe-iOS
+//
+//  Created by 송지훈 on 2022/05/07.
+//
+
+import UIKit
+
+class FeedListEmptyView: XibView{
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+}
